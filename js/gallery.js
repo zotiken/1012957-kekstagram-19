96b0834d8d80b5000730903b13eefc146f298168
nikita <zotiken@gmail.com>
2020-02-26 21:10:20 +0300

    module6-task3

---
+++ js/gallery.js
@@ -0,0 +1,30 @@
+'use strict';
+(function () {
+
+  var templatePicture = document.querySelector('#picture')
+    .content.
+  querySelector('.picture');
+
+
+  var pictureBlockGeneration = function (params) {
+
+    var pictureElement = templatePicture.cloneNode(true);
+
+    pictureElement.querySelector('.picture__img').src = params.url;
+    pictureElement.querySelector('.picture__likes').textContent = params.likes;
+    pictureElement.querySelector('.picture__comments').textContent = params.comments.length;
+
+    return pictureElement;
+  };
+
+  var onGetDescriptionPhotos = function (data) {
+
+    var fragment = document.createDocumentFragment();
+    for (var i = 0; i < data.length; i++) {
+      fragment.appendChild(pictureBlockGeneration(data[i]));
+    }
+    document.querySelector('.pictures').appendChild(fragment);
+  };
+
+  window.backend.load('https://js.dump.academy/kekstagram/data', onGetDescriptionPhotos, window.main.errorBlockGeneration);
+})();
---
+++ js/preview.js
@@ -0,0 +1,37 @@
+'use strict';
+(function () {
+  var bigPicture = document.querySelector('.big-picture');
+
+  var bigPictureImgCancel = document.querySelector('.big-picture__cancel');
+
+  var openBigImageTab = function (elem) {
+    bigPicture.classList.remove('hidden');
+    document.querySelector('.likes-count').textContent = elem.querySelector('.picture__likes').textContent;
+    bigPicture.querySelector('img').src = elem.querySelector('.picture__img').src;
+    document.querySelector('.comments-count').textContent = elem.querySelector('.picture__comments').textContent;
+  };
+
+
+  var onopenBigImage = function (evt) {
+    if (evt.target.classList[0] === 'picture' || evt.target.classList[0] === 'picture__img') {
+      if (evt.target.classList[0] === 'picture__img') {
+        openBigImageTab(evt.path[1]);
+      } else {
+        openBigImageTab(evt.target);
+      }
+    }
+  };
+
+  document.querySelector('.pictures').addEventListener('click', onopenBigImage);
+
+
+  bigPictureImgCancel.addEventListener('click', function () {
+    bigPicture.classList.add('hidden');
+  });
+  document.addEventListener('keydown', function (evt) {
+    if (evt.keyCode === 27) {
+      bigPicture.classList.add('hidden');
+    }
+  });
+
+})();
